17c44937b7020528e5c79f1efdbaf83595b939f4
trob0697 <tahreak.robinson@gmail.com>
2023-06-06 11:02:08 -0400

    add test data

--- front-end/assets/test_data.tsx
+++ front-end/assets/test_data.tsx
@@ -7,7 +7,17 @@ import * as Enums from "../helper/enums";
 import * as Interfaces from "../helper/interfaces";
 
 export const PreviousSessionsTestData: Interfaces.PreviousSession[] = [
-
+  {
+    uuid: uuidv4(),
+    location: "Tampa Hard Rock Casino",
+    gameType: Enums.GameTypes.NoLimitHoldem,
+    smallBlind: 1,
+    bigBlind: 2,
+    cashIn: 200,
+    cashOut: 400,
+    start: new Date(2023, 5, 5, 10, 0, 0, 0),
+    end: new Date(2023, 5, 5, 18, 0, 0, 0),
+  },
   {
     uuid: uuidv4(),
     location: "Tampa Hard Rock Casino",
